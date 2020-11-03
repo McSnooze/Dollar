374e06b92399f0369827971daa1ecf5df954aec6
McSnooze <McSnooze@users.noreply.github.com>
2020-11-03 10:25:38 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,14 @@ import PackageDescription
 
 let package = Package(
   name: "Dollar",
+  platforms: [
+    .macOS(.v10_10), .iOS(.v9), .tvOS(.v9), .watchOS(.v3)
+  ],
   products: [
     .library(name: "Dollar", targets: ["Dollar"])
-  ]
+  ],
+  targets: [
+    .target(name: "Dollar", dependencies: [])
+  ],
+  swiftLanguageVersions: [.v5]
 )
